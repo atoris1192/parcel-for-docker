978d7f0bc8da5ec18f316b7e5ce0dd793e976eb6
atoris1192 <atoris1192@gmail.com>
2019-10-01 20:06:42 +0900

    li

--- src/Application.tsx
+++ src/Application.tsx
@@ -5,16 +5,34 @@ interface Todos {
   title: String;
   isDone: Boolean;
 }
-
 const todos: Todos[] = [
   { id: 0, title: "Task 1", isDone: false },
   { id: 1, title: "Task 2", isDone: false },
   { id: 3, title: "Task 3", isDone: true },
 ]
 
+
+function TodoItem(props) {
+  return(
+    <li key={ props.todo.id }>
+      <span><input type="checkbox"
+        checked={ props.todo.isDone }
+        onChange={ () => { props.checkItem(props.todo) }} 
+      /></span>
+      { props.todo.title }
+      <button>Del</button>
+    </li>
+  )
+}
+
+
 function TodoList (props) {
   const todos = props.todos.map( todo => {
-    return(<li key={ todo.id }>{ todo.title }</li>)
+    return TodoItem(
+      todo={ todo }
+      checkItem={props.checkItem}
+    )
+    // return(<li key={ todo.id }>{ todo.title }</li>)
   })
   return(
     <ul>
@@ -23,6 +41,7 @@ function TodoList (props) {
   )
 }
 
+
 class App extends React.Component {
   private state: any;
   constructor() {
@@ -30,11 +49,20 @@ class App extends React.Component {
     this.state = {
       todos: todos,
     }
+    this.checkItem = this.checkItem.bind(this);
+  }
+  checkItem(props) {
+    
+    return
   }
   render() {
     return(
       <div className="container">
-        <TodoList todos={this.state.todos}/>
+        <h1>Todos</h1>
+        <TodoList
+          todos={this.state.todos}
+          checkItem={ this.checkItem }
+        />
       </div>
     )
   }
