8aca5fb4d67d50a8d081cbb5d5885c23f56455cf
atoris1192 <atoris1192@gmail.com>
2019-10-01 18:11:44 +0900

    todoList

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,12 +1,30 @@
 import * as React from 'react';
 
-const todos = [
+interface Todos {
+  id: Number | String;
+  title: String;
+  isDone: Boolean;
+}
+
+const todos: Todos[] = [
   { id: 0, title: "Task 1", isDone: false },
   { id: 1, title: "Task 2", isDone: false },
   { id: 3, title: "Task 3", isDone: true },
 ]
 
+function TodoList (props) {
+  const todos = props.todos.map( todo => {
+    return(<li key={ todo.id }>{ todo.title }</li>)
+  })
+  return(
+    <ul>
+      { todos }
+    </ul>
+  )
+}
+
 class App extends React.Component {
+  private state: any;
   constructor() {
     super();
     this.state = {
@@ -16,14 +34,10 @@ class App extends React.Component {
   render() {
     return(
       <div className="container">
-        <p>{ this.state.todos[0].title }</p>
-        <p>{ this.state.todos[1].title }</p>
-        <p>{ this.state.todos[2].title }</p>
-        
+        <TodoList todos={this.state.todos}/>
       </div>
     )
   }
-
 }
 
 export default App
